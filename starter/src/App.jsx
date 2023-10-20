13b635afd3dbe1f1e6a71b002226f1265e33826e
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-20 18:04:43 -0400

    iterate and render

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -1,5 +1,31 @@
+import { useState } from 'react';
 import data from './data';
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [person, setPerson] = useState(data);
+  const numBirthdays = person.length;
+  const handleClick = () => {
+    setPerson([]);
+  };
+  return (
+    <main>
+      <div className="container">
+        <h3>{numBirthdays} birthdays today</h3>
+        {person.map((item) => {
+          const { id, name, age, image } = item;
+          return (
+            <div className="person" key={id}>
+              <img src={image} alt="" />
+              <h4>{name}</h4>
+              <p>{age}</p>
+            </div>
+          );
+        })}
+        <button className="btn" onClick={handleClick}>
+          clear all
+        </button>
+      </div>
+    </main>
+  );
 };
 export default App;
